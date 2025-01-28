0142c8253c325e1ce7e9409c4990a021e755e772
Edilson Emidio <edilsonemidio93@gmail.com>
2025-01-28 17:10:42 -0300

    pronto para deploy

--- src/main/java/com/devsuperior/dslist/servicies/GameService.java
+++ src/main/java/com/devsuperior/dslist/servicies/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -23,9 +24,18 @@ public class GameService {
 		GameDTO dto = new GameDTO(result);
 		return dto;
 	}
+	
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> list = result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+		return list;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		List<GameMinDTO> list = result.stream().map(x -> new GameMinDTO(x)).toList();
 		
 		return list;
